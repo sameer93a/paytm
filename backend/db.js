4930331ad5a73f5eab4ff1f7b0990a862054d2d2
Sameer Ahmed <sameer937655@gmail.com>
2024-08-30 13:10:28 +0530

    enhance the db.js

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,52 @@
 const mongoose = require("mongoose");
-const { string } = require("zod");
+
+mongoose.connect("");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  firstName: String,
-  lastName: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
 });
 
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Account = mongoose.model("Account", accountSchema);
 const User = mongoose.model("User", userSchema);
 
 module.exports = {
   User,
+  Account,
 };
--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const { authMiddleware } = require("../middleware");
+const { Account } = require("../db");
+const { default: mongoose } = require("mongoose");
 
+const router = express.Router();
+
+router.get("/balance", authMiddleware, async (req, res) => {
+  const account = await Account.findOne({
+    userId: req.userId,
+  });
+  res.json({
+    balance: account.balance,
+  });
+});
